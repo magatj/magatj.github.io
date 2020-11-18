94ac54dcce1de4130e4ab7ba204c6297e2b5ffca
magatj <48863783+magatj@users.noreply.github.com>
2020-11-18 15:18:45 -0800

    Update conf.py
    changed back to 'sphinxcontrib.programoutput'

--- sphinx_doc/source/conf.py
+++ sphinx_doc/source/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax'
-   ,'sphinxcontrib.commandoutput'
+   ,'sphinxcontrib.programoutput'
 	
 	#,'nbsphinx'
 ]
